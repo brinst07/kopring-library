8b61cc13fa0db41515f0550644c97c02ef049050
brinst07 <brinst07@gmail.com>
2023-04-15 20:49:03 +0900

    sector 4

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,7 +1,10 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByName(name: String) : User?
+    @Query("select DISTINCT u from User u left join fetch u.userLoanHIstory")
+    fun findAllWithHisotries() : List<User>
 }
